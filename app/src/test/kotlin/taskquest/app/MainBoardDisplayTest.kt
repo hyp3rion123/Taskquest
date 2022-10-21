67eac5f62ed0c15f2561e6078ca8907cc2c317b2
matthewswitt <54003058+matthewswitt@users.noreply.github.com>
2022-10-21 00:42:39 -0400

    remove junit testing for ui

--- app/src/test/kotlin/taskquest/app/MainBoardDisplayTest.kt
+++ app/src/test/kotlin/taskquest/app/MainBoardDisplayTest.kt
@@ -8,22 +8,22 @@ import javafx.scene.control.Button
 
 class MainBoardDisplayTest {
     //Simulating button-Create task
-    @Test
-    fun addTask() {
-        var display = MainBoardDisplayTest()
-        var taskList1 = TaskList(1, "TaskList1", "This is a test task list")
-        for (id in 1..10) {
-            var task = Task(id=id,title="Task $id",desc="some desc", complete = (id % 2 == 0))
-            taskList1.addItem(task)
-        }
-        var create_button = Button()
-        var vbox = display.createTasksVBox(create_button, taskList1)
-
-        //Simulate add a task
-        var task = Task(1, "title", "desc", "due", "created")
-        create_button.fire()
-
-    }
+//    @Test
+//    fun addTask() {
+//        var display = MainBoardDisplayTest()
+//        var taskList1 = TaskList(1, "TaskList1", "This is a test task list")
+//        for (id in 1..10) {
+//            var task = Task(id=id,title="Task $id",desc="some desc", complete = (id % 2 == 0))
+//            taskList1.addItem(task)
+//        }
+//        var create_button = Button()
+//        var vbox = display.createTasksVBox(create_button, taskList1)
+//
+//        //Simulate add a task
+//        var task = Task(1, "title", "desc", "due", "created")
+//        create_button.fire()
+//
+//    }
 
 
 
