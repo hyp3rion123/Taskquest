c99f64eaa786300e96b3af647bf31eb0a7cebee7
William Wang <william.wang1@uwaterloo.ca>
2022-11-11 11:58:42 -0500

    Working Profile screen stats

--- utilities/src/main/kotlin/taskquest/utilities/models/User.kt
+++ utilities/src/main/kotlin/taskquest/utilities/models/User.kt
@@ -4,6 +4,9 @@ data class User(var lastUsedList: Int = - 1, var wallet: Int = 0) {
     val lists = mutableListOf<TaskList>()
     val purchasedItems = mutableListOf<Item>()
     val tags = mutableSetOf<String>()
+    var profileImageName: String = "Default.png"
+    var longestStreak: Int = 0
+    var level: Int = 0
 
     fun convertToString() {
         for (list in lists) {
