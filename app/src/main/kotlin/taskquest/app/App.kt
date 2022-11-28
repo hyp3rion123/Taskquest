be3e1bd6412f998bbf7e956ef8c64bc1beddd9e9
William Wang <bn.william.w@gmail.com>
2022-11-27 19:45:54 -0500

    Commented out errors

--- app/src/main/kotlin/taskquest/app/App.kt
+++ app/src/main/kotlin/taskquest/app/App.kt
@@ -3,14 +3,17 @@
  */
 package taskquest.app
 
+//import com.dustinredmond.fxtrayicon.FXTrayIcon
 import javafx.application.Application
 import javafx.stage.Stage
 import taskquest.app.javafx.*
-
 class App: Application() {
     override fun start(stage: Stage?) {
         val display = MainBoardDisplay()
         display.start_display(stage)
+
+        //val icon = FXTrayIcon(stage, javaClass.getResource("someImageFile.png"))
+        //icon.show()
     }
 }
 
